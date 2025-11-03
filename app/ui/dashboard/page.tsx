1734c26705ff4b829f82d6dcafe7c514a821bb0c
Kestutis Eirosius <eirosiuss@gmail.com>
2025-11-03 17:35:44 +0200

    trying neon

--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -1,20 +1,18 @@
 import Image from "next/image";
+import ImageDesktop from "../../../public/home/desktop/image-hero.jpg";
+import ImageTablet from "../../../public/home/tablet/image-header.jpg";
 
 export default function HomePage() {
   return (
     <>
       <p>Home Page</p>
       <Image
-        src="/home/desktop/image-hero.jpg"
-        width={1440}
-        height={729}
+        src={ImageDesktop}
         className="hidden lg:block"
         alt="XX99 Mark II Headphones"
       />
       <Image
-        src="/home/tablet/image-header.jpg"
-        width={1536}
-        height={1458}
+        src={ImageTablet}
         className="lg:hidden"
         alt="XX99 Mark II Headphones"
       />

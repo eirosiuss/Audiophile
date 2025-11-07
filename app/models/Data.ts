1ef354a04b2b3deea3df47ad5c6d0ca3ae372d41
Kestutis Eirosius <eirosiuss@gmail.com>
2025-11-07 14:47:04 +0200

    add mongoose model

---
+++ app/models/Data.ts
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+export interface Product extends mongoose.Document {
+  slug: string;
+  name: string;
+  image: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  category: string;
+  categoryImage: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  new: boolean;
+  price: number;
+  description: string;
+  features: string;
+  includes: {
+    quantity: number;
+    item: string;
+  }[];
+  gallery: {
+    first: { mobile: string; tablet: string; desktop: string };
+    second: { mobile: string; tablet: string; desktop: string };
+    third: { mobile: string; tablet: string; desktop: string };
+  };
+  others: {
+    slug: string;
+    name: string;
+    image: { mobile: string; tablet: string; desktop: string };
+  }[];
+}
+
+const ProductSchema = new mongoose.Schema<Product>({
+  slug: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: {
+    mobile: String,
+    tablet: String,
+    desktop: String,
+  },
+  category: { type: String, required: true },
+  categoryImage: {
+    mobile: String,
+    tablet: String,
+    desktop: String,
+  },
+  new: { type: Boolean, default: false },
+  price: { type: Number, required: true },
+  description: String,
+  features: String,
+  includes: [
+    {
+      quantity: Number,
+      item: String,
+    },
+  ],
+  gallery: {
+    first: { mobile: String, tablet: String, desktop: String },
+    second: { mobile: String, tablet: String, desktop: String },
+    third: { mobile: String, tablet: String, desktop: String },
+  },
+  others: [
+    {
+      slug: String,
+      name: String,
+      image: { mobile: String, tablet: String, desktop: String },
+    },
+  ],
+});
+
+export default mongoose.models.Product ||
+  mongoose.model<Product>("Product", ProductSchema);

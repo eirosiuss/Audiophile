ea64f30948de40720833ce284e0ee4f3124a9778
Kestutis Eirosius <eirosiuss@gmail.com>
2025-11-27 10:44:55 +0200

    add tests to navigation

--- __tests__/navigation/Nav.test.jsx
+++ __tests__/navigation/Nav.test.jsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Nav from "@/ui/nav";
 import { usePathname } from "next/navigation";
-import userEvent from '@testing-library/user-event'
+import userEvent from "@testing-library/user-event";
 
 jest.mock("next/navigation", () => ({
   usePathname: jest.fn(),
@@ -12,25 +12,21 @@ describe("Nav", () => {
     render(<Nav />);
     expect(screen.getAllByRole("navigation")).toHaveLength(2);
   });
+
   it("should show nav links", () => {
     render(<Nav />);
-    const homeLink = screen.getByRole("link", {
-      name: /home/i,
+    const links = [
+      { name: "home", href: "/home" },
+      { name: "headphones", href: "/home/headphones" },
+      { name: "speakers", href: "/home/speakers" },
+      { name: "earphones", href: "/home/earphones" },
+    ];
+    links.forEach(({ name, href }) => {
+      const link = screen.getByRole("link", { name: new RegExp(name, "i") });
+      expect(link).toHaveAttribute("href", href);
     });
-    const headphonesLink = screen.getByRole("link", {
-      name: /headphones/i,
-    });
-    const speakersLink = screen.getByRole("link", {
-      name: /speakers/i,
-    });
-    const earphonesLink = screen.getByRole("link", {
-      name: /earphones/i,
-    });
-    expect(homeLink).toHaveAttribute("href", "/home");
-    expect(headphonesLink).toHaveAttribute("href", "/home/headphones");
-    expect(speakersLink).toHaveAttribute("href", "/home/speakers");
-    expect(earphonesLink).toHaveAttribute("href", "/home/earphones");
   });
+
   it("should apply active class to selected link", () => {
     usePathname.mockReturnValue("/home");
     render(<Nav />);
@@ -40,9 +36,20 @@ describe("Nav", () => {
     expect(homeLink).toHaveClass("bg-sky-100");
     expect(homeLink).toHaveClass("text-blue-600");
   });
-  it("should open mobile menu on click", () => {
-  render(<Nav />);
-  const user = userEvent.setup();
 
-});
+  it("should show mobile-tablet menu on click", async () => {
+    render(<Nav />);
+    const user = userEvent.setup();
+    const menuBtn = screen.getByRole("button", {
+      name: /menu/i,
+    });
+    await user.click(menuBtn);
+    const headings = ["headphones", "speakers", "earphones"];
+
+    headings.forEach((element) => {
+      expect(
+        screen.getByRole("heading", { name: new RegExp(element, "i") })
+      ).toBeInTheDocument();
+    });
+  });
 });
